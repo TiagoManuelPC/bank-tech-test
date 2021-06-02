c9cd6083b3608473d5f5b4c8217f83bf3b5c51aa
TiagoManuelPC <tiagomanuelpereiracampos@hotmail.com>
2021-06-02 16:59:41 +0100

    changed subject name on bank_account spec to a clear one

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -3,47 +3,47 @@
 require 'bank_account'
 
 describe BankAccount do
-  subject(:tiago) { described_class.new }
+  subject(:new_account) { described_class.new }
   let(:statement) { double :statement, print: 'a string' }
 
   it 'can create a new account' do
-    expect(tiago).to be_an_instance_of(BankAccount)
+    expect(new_account).to be_an_instance_of(BankAccount)
   end
 
   it 'starts with a balance of zero' do
-    expect(tiago.balance).to be_equal(0)
+    expect(new_account.balance).to be_equal(0)
   end
 
   context '#has transactions that' do
     before do
-      tiago.deposit(500)
+      new_account.deposit(500)
     end
 
     it 'allow deposits' do
-      tiago.deposit(500)
-      expect(tiago.balance).to be_equal(1000)
+      new_account.deposit(500)
+      expect(new_account.balance).to be_equal(1000)
     end
 
     it 'allow to withdraw money' do
-      tiago.withdraw(500)
-      expect(tiago.balance).to be_equal(0)
+      new_account.withdraw(500)
+      expect(new_account.balance).to be_equal(0)
     end
 
     it 'doesnt put the account in overdraft' do
-      expect { tiago.withdraw(1000) }.to raise_error("Can't complete transaction: Insufficient Funds")
+      expect { new_account.withdraw(1000) }.to raise_error("Can't complete transaction: Insufficient Funds")
     end
   end
 
   context 'stores transactions data' do
     it 'starts with no transactions' do
-      expect(tiago.transactions).to be_empty
+      expect(new_account.transactions).to be_empty
     end
   end
 
   describe '#print_statement' do
     it 'delagates to the Statement object' do
       expect(statement).to receive(:print)
-      tiago.print_statement(statement)
+      new_account.print_statement(statement)
     end
   end
 end
